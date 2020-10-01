bb0f3ec2f8143c080dc18550fae1614b408bc90e
kirill ginko <kirillginko@gmail.com>
2020-09-30 21:04:37 -0400

    initialized express

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const Datastore = require('nedb');
-
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
