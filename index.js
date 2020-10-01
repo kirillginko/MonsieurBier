c65a97490f9d83bd393d997efb3c402b7baafd56
kirill ginko <kirillginko@gmail.com>
2020-09-30 20:57:11 -0400

    created app

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const Datastore = require('nedb');
+
+const app = express();
+app.listen(3000, () => console.log('listening at 3000'));
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
