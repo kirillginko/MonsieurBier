6e64445c3213fc6857229f40dc8d57c9a3088cf4
kirill ginko <kirillginko@gmail.com>
2020-10-11 15:03:12 -0400

    added port to index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 require('dotenv').config();
 
 // console.log(process.env)
-app.listen(3000, () => console.log('listening at 3000'));
+app.listen(port, () => {
+  console.log(`Starting server at ${port}`);
+});
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
